48177d84a4c264e6f1ec57f85fb2031a26d6bcfb
Yuxiang (Sean) Li <seanliyuxiang@gmail.com>
2020-09-19 00:01:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,12 +4,12 @@ function basicTeenager(age) {
   }
 }
 
-// function teenager(age) {
-//   if (age >= 13 && age <= 19) {
-//     return "You are a teenager!";
-//   }
-//   return "You are not a teenager";
-// }
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+  return "You are not a teenager";
+}
 //
 // function ageChecker(age) {
 //   if (age >= 13 && age <= 19) {
