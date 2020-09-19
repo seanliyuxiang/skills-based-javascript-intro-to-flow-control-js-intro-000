51164fa62facd11c485239a0a0fe0f640f024b66
Yuxiang (Sean) Li <seanliyuxiang@gmail.com>
2020-09-19 00:00:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,7 +10,7 @@ function basicTeenager(age) {
 //   }
 //   return "You are not a teenager";
 // }
-// 
+//
 // function ageChecker(age) {
 //   if (age >= 13 && age <= 19) {
 //         return "You are a teenager!";
@@ -20,15 +20,15 @@ function basicTeenager(age) {
 //         return "You are a grownup";
 //     }
 // }
-// 
+//
 // function ternaryTeenager(age) {
 //   return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
 // }
-// 
+//
 // // Define a function switchAge that accepts an age as a parameter.
 // // The case statement should switch on age and return "You are a teenager"
 // // if the age is 13, 14, 15, 16, 17, 18, or 19, and return "You have an age" as the default.
-// 
+//
 // function switchAge(age) {
 //   switch age {
 //     case 13:
